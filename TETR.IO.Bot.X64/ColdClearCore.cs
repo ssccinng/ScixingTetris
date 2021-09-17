b1de67f091d602fdebe100c4eadb8d161543d6da
ssccinng <whs11998@163.com>
2021-09-17 13:33:24 +0800

    CCbot

---
+++ TETR.IO.Bot.X64/ColdClearCore.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+namespace TETR.IO.Bot.X64
+{
+    public struct CCBook { }
+    [StructLayout(LayoutKind.Sequential, Pack = 4)]
+    public struct CCWeights
+    {
+        public Int32 back_to_back { get; set; }
+        public Int32 bumpiness { get; set; }
+        public Int32 bumpiness_sq { get; set; }
+        public Int32 row_transitions { get; set; }
+        public Int32 height { get; set; }
+        public Int32 top_half { get; set; }
+        public Int32 top_quarter { get; set; }
+        public Int32 jeopardy { get; set; }
+        public Int32 cavity_cells { get; set; }
+        public Int32 cavity_cells_sq { get; set; }
+        public Int32 overhang_cells { get; set; }
+        public Int32 overhang_cells_sq { get; set; }
+        public Int32 covered_cells { get; set; }
+        public Int32 covered_cells_sq { get; set; }
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+        [JsonInclude]
+        public Int32[] tslot;// = new Int32[4] { get; set; }
+        //public Int32[] Tslot => tslot;
+        public Int32 well_depth { get; set; }
+        public Int32 max_well_depth { get; set; }
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 10)]
+        [JsonInclude]
+        public Int32[] well_column; // = new Int32[10] { get; set; }
+        //public Int32[] Well_column => well_column;
+        public Int32 b2b_clear { get; set; }
+        public Int32 clear1 { get; set; }
+        public Int32 clear2 { get; set; }
+        public Int32 clear3 { get; set; }
+        public Int32 clear4 { get; set; }
+        public Int32 tspin1 { get; set; }
+        public Int32 tspin2 { get; set; }
+        public Int32 tspin3 { get; set; }
+        public Int32 mini_tspin1 { get; set; }
+        public Int32 mini_tspin2 { get; set; }
+        public Int32 perfect_clear { get; set; }
+        public Int32 combo_garbage { get; set; }
+        public Int32 move_time { get; set; }
+        public Int32 wasted_t { get; set; }
+        public byte use_bag { get; set; }
+        public byte timed_jeopardy { get; set; }
+        public byte stack_pc_damage { get; set; }
+
+        
+       
+    }
+    public enum CCMovementMode
+    {
+        CC_0G,
+        CC_20G,
+        CC_HARD_DROP_ONLY
+    }
+
+    public enum CCSpawnRule
+    {
+        CC_ROW_19_OR_20,
+        CC_ROW_21_AND_FALL,
+    }
+    public enum CCMovement
+    {
+        CC_LEFT, CC_RIGHT,
+        CC_CW, CC_CCW,
+        /* Soft drop all the way down */
+        CC_DROP
+    }
+    public enum CCTspinStatus
+    {
+        CC_NONE_TSPIN_STATUS,
+        CC_MINI,
+        CC_FULL,
+    }
+    public enum CCPiece
+    {
+        //CC_I, CC_T, CC_O, CC_S, CC_Z, CC_L, CC_J
+        CC_I, CC_O, CC_T, CC_L, CC_J, CC_S, CC_Z
+    }
+
+    public enum CCPcPriority
+    {
+        CC_PC_OFF,
+        CC_PC_FASTEST,
+        CC_PC_ATTACK
+    }
+
+    public enum CCBotPollStatus
+    {
+        CC_MOVE_PROVIDED,
+        CC_WAITING,
+        CC_BOT_DEAD
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 4)]
+    public struct CCPlanPlacement
+    {
+        CCPiece piece;
+        CCTspinStatus tspin;
+
+        /* Expected cell coordinates of placement, (0, 0) being the bottom left */
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+        public byte[] expected_x;
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+        public byte[] expected_y;
+
+        /* Expected lines that will be cleared after placement, with -1 indicating no line */
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+        Int32[] cleared_lines;
+    }
+
+
+
+    [StructLayout(LayoutKind.Sequential, Pack = 4)]
+
+    public struct CCMove
+    {
+        /* Whether hold is required */
+
+        //public bool  hold;
+        public byte hold;
+        //public char hold1;
+        /* Expected cell coordinates of placement, (0, 0) being the bottom left */
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+        public byte[] expected_x;
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+        public byte[] expected_y;
+        ///* Number of moves in the path */
+
+        public byte movement_count;
+        //public char movement_count1;
+        ///* Movements */
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 32)]
+        public CCMovement[] movements;
+
+        ///* Bot Info */
+
+        public uint nodes;
+
+        public uint depth;
+
+        public uint original_rank;
+
+    }
+    [StructLayout(LayoutKind.Sequential, Pack = 4)]
+    public struct CCOptions
+    {
+        public CCMovementMode mode { get; set; }
+        public CCSpawnRule spawn_rule { get; set; }
+        public CCPcPriority PcPriority { get; set; }
+
+
+        public UInt32 min_nodes { get; set; }
+        public UInt32 max_nodes { get; set; }
+        public UInt32 threads { get; set; }
+        public byte use_hold { get; set; }
+        //public char use_hold1 { get; set; }
+        public byte speculate { get; set; }
+        //public char speculate1 { get; set; }
+    }
+
+    public struct CCAsyncBot { }
+    public static class ColdClearCore
+    {
+
+        // [///DllImport("cold_clear.dll", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("cold_clear.dll")]
+        public static extern IntPtr cc_launch_async(CCOptions options, CCWeights weights, CCBook book, CCPiece[] queue, UInt32 count);
+
+        [DllImport("cold_clear.dll")]
+        public static extern IntPtr cc_launch_with_board_async( CCOptions options, CCWeights weights, byte[] field,
+    UInt32 bag_remain, CCPiece hold/*？？？*/, byte b2b, UInt32 combo, CCPiece[] queue, UInt32 count);
+        //public static extern IntPtr cc_launch_async(IntPtr options, IntPtr weights);
+        [DllImport("cold_clear.dll")]
+        public static extern void cc_destroy_async(IntPtr bot);
+        [DllImport("cold_clear.dll")]
+        public static extern void cc_reset_async(IntPtr bot, byte[] field, byte b2b, UInt32 combo);
+        [DllImport("cold_clear.dll")]
+        public static extern void cc_add_next_piece_async(IntPtr bot, CCPiece piece);
+        [DllImport("cold_clear.dll")]
+        public static extern void cc_request_next_move(IntPtr bot, UInt32 incoming);
+        //[DllImport("cold_clear.dll")]
+        //public static extern int cc_poll_next_move(IntPtr bot, IntPtr move);
+        [DllImport("cold_clear.dll")]
+        public static extern CCBotPollStatus cc_poll_next_move(IntPtr bot, ref CCMove move, CCPlanPlacement[] plan, IntPtr plan_length);
+
+
+        [DllImport("cold_clear.dll")]
+        public static extern CCBotPollStatus cc_block_next_move(
+            IntPtr bot,
+            IntPtr move,
+            CCPlanPlacement[] plan,
+            IntPtr plan_length
+        );
+        [DllImport("cold_clear.dll")]
+        public static extern bool cc_is_dead_async(IntPtr bot);
+        //[DllImport("cold_clear.dll")]
+        //public static extern void cc_default_options(IntPtr options);
+
+        //[DllImport("cold_clear.dll")]
+        //public static extern void cc_default_weights(IntPtr weights);
+        
+        [DllImport("cold_clear.dll")]
+        public static extern void cc_default_options(ref CCOptions options);
+
+        [DllImport("cold_clear.dll")]
+        public static extern void cc_default_weights(ref CCWeights weights);
+
+        [DllImport("cold_clear.dll")]
+        public static extern void cc_fast_weights(ref CCWeights weights);
+
+
+    }
+}
---
+++ TETR.IO.Bot.X64/IOBot.cs
@@ -0,0 +1,280 @@
+﻿using Carter;
+using ScixingTetrisCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Carter.ModelBinding;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System.Runtime.InteropServices;
+
+namespace TETR.IO.Bot.X64
+{
+    public class MoveResult
+    {
+        public bool hold { get; set; }
+        public List<string> moves { get; set; } = new List<string>();
+        public int[][] expected_cells { get; set; }
+
+    }
+    public enum WeightType
+    {
+        None,
+        Default,
+        Fast,
+    }
+    public class BotSetting
+    {
+        public int NextCnt { get; set; } = 6;
+        public int BPM { get; set; } = 200;
+        [JsonInclude]
+        public CCWeights CCWeights;
+        [JsonInclude]
+        public CCOptions CCOptions;
+        public WeightType WeightType { get; set; } = WeightType.None;
+        //public 
+        //public bool AutoLevel { get; set; } = true;
+
+    }
+    public class IOBot : CarterModule
+    {
+        static Queue<CCPiece> _nextQueue = new();
+        static IntPtr _bot = IntPtr.Zero;
+        static int _pieceCnt = 0;
+        static object _lockQueue = new();
+        static object _lockBot = new();
+        static BotSetting _botSetting = new BotSetting();
+
+        static DateTime _startTime;
+        static int _nowIdx = 0;
+        public IOBot()
+        {
+            Post("/newGame", async (req, res) =>
+            {
+
+                Init();
+                var nextQueue = await req.Bind<string[]>();
+                AddNext(nextQueue);
+                Console.WriteLine("新的一局开始了！");
+                Console.WriteLine($"序列为！{string.Join(",", nextQueue[..20])}...");
+
+            });
+
+            Post("/endGame", async (req, res) =>
+            {
+                if (_bot == IntPtr.Zero) return;
+                ColdClearCore.cc_destroy_async(_bot);
+                Console.WriteLine("游戏结束！");
+            });
+
+            Post("/newPieces", async (req, res) =>
+            {
+                if (_bot == IntPtr.Zero) return;
+                var nextQueue = await req.Bind<string[]>();
+                AddNext(nextQueue);
+                Console.WriteLine("添加新序列");
+            });
+            Get("/GetPieces", async (req, res) =>
+            {
+                //await res.WriteAsJsonAsync(_IOBoard.NextQueue);
+            });
+            Post("/nextMove", async (req, res) =>
+            {
+                Console.WriteLine("请求收到了");
+                if (_bot == IntPtr.Zero) return;
+                var garbage = await req.Bind<int>();
+                // 重置
+                await res.WriteAsJsonAsync(GetMove(garbage));
+
+            });
+
+            Post("/resetBoard", async (req, res) =>
+            {
+                if (_bot == IntPtr.Zero) return;
+                var result = await req.Bind<JsonDocument>();
+                resetBoard(result);
+
+
+                //resetBorad(board);
+                Console.WriteLine("重置地图");
+            });
+            Post("/pendingGarbage", async (req, res) =>
+            {
+                if (_bot == IntPtr.Zero) return;
+                var nextQueue = req.BindAndValidate<string[]>();
+                Console.WriteLine("重置游戏红条");
+            });
+        }
+
+        private void Init()
+        {
+            _nextQueue = new();
+            CCWeights cCWeights = new CCWeights();
+            CCOptions cCOptions = new CCOptions();
+
+            Console.WriteLine(1);
+
+            
+            
+
+
+            _botSetting.CCOptions.max_nodes = 100000;
+            
+            _nowIdx = 0;
+            try
+            {
+                var options = new JsonSerializerOptions
+                {
+                    ReadCommentHandling = JsonCommentHandling.Skip,
+                    AllowTrailingCommas = true,
+                };
+                _botSetting = JsonSerializer.Deserialize<BotSetting>(System.IO.File.ReadAllText("TetrSetting.json"), options);
+            }
+            catch (Exception)
+            {
+                var options = new JsonSerializerOptions
+                {
+                    WriteIndented = true,
+                };
+                ColdClearCore.cc_default_options(ref _botSetting.CCOptions);
+                ColdClearCore.cc_default_weights(ref _botSetting.CCWeights);
+                System.IO.File.WriteAllTextAsync("TetrSetting.json", JsonSerializer.Serialize(_botSetting, options));
+            }
+            
+            if (_botSetting.WeightType == WeightType.Default)
+            {
+                
+                ColdClearCore.cc_default_weights(ref _botSetting.CCWeights);
+            }
+            else if (_botSetting.WeightType == WeightType.Fast)
+            {
+                ColdClearCore.cc_fast_weights(ref _botSetting.CCWeights);
+            }
+            Console.WriteLine("数据读取完成");
+            _bot = ColdClearCore.cc_launch_async(_botSetting.CCOptions, _botSetting.CCWeights, new CCBook(), null, 0);
+            Console.WriteLine($"初始化完成: {_bot}");
+            // 读取配置文件
+        }
+
+        private void AddNext(string[] nextQueue)
+        {
+            foreach (string mino in nextQueue)
+            {
+                _nextQueue.Enqueue((CCPiece)Enum.Parse(typeof(CCPiece), "CC_" + mino));
+            }
+            Console.WriteLine("Next加入队列...");
+            UpdateNext();
+
+        }
+
+        private void UpdateNext()
+        {
+            lock (_lockBot)
+            {
+                Console.WriteLine(_nextQueue.Count);
+                while (_pieceCnt < _botSetting.NextCnt && _nextQueue.Count > 0)
+                {
+                    ColdClearCore.cc_add_next_piece_async(_bot, _nextQueue.Dequeue());
+                    _pieceCnt++;
+                }
+                Console.WriteLine("Next更新完成...");
+            }
+        }
+
+        private void resetBoard(JsonDocument board)
+        {
+
+            if (board is null) return;
+            JsonElement data = board.RootElement.GetProperty("board");
+            byte[] ff = new byte[400];
+            for (int i = 39; i >= 0; --i)
+            {
+                for (int j = 0; j < 10; ++j)
+                {
+                    ff[(39 - i) * 10 + j] = (byte)(data[i][j].GetString() == null ? 0 : 1);
+                }
+            }
+            
+            lock (_lockBot)
+            {
+                ColdClearCore.cc_reset_async(_bot, ff, 0, 0);
+            }
+
+        }
+
+
+        private MoveResult GetMove(int garbage)
+        {
+            MoveResult moveResult = new MoveResult();
+            
+            lock (_lockBot)
+            {
+                Console.WriteLine("开始请求调用");
+                ColdClearCore.cc_request_next_move(_bot, (uint)garbage);
+                Console.WriteLine("请求调用成功");
+                CCMove cCMove = new CCMove();
+                CCBotPollStatus aa;
+                while ((aa = ColdClearCore.cc_poll_next_move(_bot, ref cCMove, null, IntPtr.Zero)) != CCBotPollStatus.CC_MOVE_PROVIDED)
+                {
+                    Console.WriteLine(aa);
+                    Task.Delay(50).Wait();
+                }
+                Console.WriteLine(aa);
+                Console.WriteLine("开始写入操作");
+                for (int i = 0; i < cCMove.movement_count; ++i)
+                {
+                    switch (cCMove.movements[i])
+                    {
+                        case CCMovement.CC_LEFT:
+                            moveResult.moves.Add("Left");
+                            break;
+                        case CCMovement.CC_RIGHT:
+                            moveResult.moves.Add("Right");
+                            break;
+                        case CCMovement.CC_CW:
+                            moveResult.moves.Add("Cw");
+                            break;
+                        case CCMovement.CC_CCW:
+                            moveResult.moves.Add("Ccw");
+                            break;
+                        case CCMovement.CC_DROP:
+                            moveResult.moves.Add("SonicDrop");
+                            break;
+                        default:
+                            break;
+                    }
+                    
+                }
+                if (cCMove.hold == 1) moveResult.hold = true;
+                moveResult.expected_cells = new int[4][];
+                for (int j = 0; j < 4; ++j)
+                {
+                    moveResult.expected_cells[j] = new int[2];
+                    moveResult.expected_cells[j][0] = cCMove.expected_x[j];
+                    moveResult.expected_cells[j][1] = cCMove.expected_y[j];
+                }
+                if (_nowIdx == 0)
+                {
+                    _startTime = DateTime.Now;
+                }
+                _nowIdx++;
+                TimeSpan hopeTime = TimeSpan.FromSeconds(60.0 / _botSetting.BPM * _nowIdx);
+                //Console.WriteLine("延时" + (_startTime + hopeTime - DateTime.Now).TotalMilliseconds + "毫秒");
+                if (DateTime.Now < _startTime + hopeTime)
+                {
+                    Task.Delay(_startTime + hopeTime - DateTime.Now).Wait();
+                }
+            }
+            
+
+            //_IOBoard.PrintBoard(WithMino:false);
+            _pieceCnt--;
+            UpdateNext();
+            return moveResult;
+        }
+    }
+}
